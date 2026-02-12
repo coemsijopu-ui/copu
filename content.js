ac9cbd116202ddb1c916dee3966f134c697d5618
coemsijopu-ui <coemsijopu@gmail.com>
2026-02-12 11:00:08 -0700

    Create content.js to extract email content from Gmail and send to backend service.

---
+++ content.js
@@ -0,0 +1,13 @@
+// content.js
+
+function extractEmailContent() {
+    // Code to extract email content from Gmail
+}
+
+function sendToBackendService(content) {
+    // Code to send content to the backend service
+}
+
+// Example usage
+const emailContent = extractEmailContent();
+sendToBackendService(emailContent);
